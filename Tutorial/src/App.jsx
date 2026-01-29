1ec9641ba9d67d5294474fe4b41b0879664814c9
Varad_Chitnis <156461984+beetlejuice0@users.noreply.github.com>
2026-01-29 23:43:58 +0530

    first commit

---
+++ Tutorial/src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Herosection from "./components/Hero";
+
+function App(){
+  return (
+    <div>
+      <Navigation/>
+      <Herosection/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Tutorial/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+const Herosection = () => {
+    return(
+        <main className="hero container">
+            <div className="hero-content">
+                <h1> Your feet deserve the best </h1>
+                <p>
+                    we are here with you Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla blanditiis, facilis at veniam fugiat, praesentium deserunt optio tempora repellat numquam accusamus qui, dolorem iste unde ratione! Id voluptatem qui at.
+                </p>
+            </div>
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button>Category</button>
+            </div>
+            <div className="shopping">
+                <p>
+                    Available on
+                </p>
+            </div>
+            <div className="Brand-icons">
+                <img src="/images/amazon.png" alt="amazon-logo" />
+                <img src="/images/flipkart.png" alt="flipkart-logo" />
+            </div>
+            <div className="hero-image">
+                <img src="/images/shoe_image.png" alt="hero-image" />
+            </div>
+        </main>
+    );
+};
+
+export default Herosection;
